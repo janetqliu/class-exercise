46a324e48e46d70254b12ee255332cbb11dc3670
janet_liu <janetqliu@gmail.com>
2026-09-18 14:07:40 -0400

    add class 2 exercise

---
+++ class2_argparse_demo.py
@@ -0,0 +1,29 @@
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(description="Analyze a data file")
+
+parser.add_argument(
+    "--input", 
+    "-i", 
+    required=True, 
+    help="Path to input CSV file"
+)
+
+parser.add_argument(
+    "--output",
+    "-o",
+    default="results.txt",
+    help="Output file path"
+)
+
+parser.add_argument(
+    "--verbose",
+    "-v",
+    action="store_true",
+    help="Print detailed information"
+)
+
+args = parser.parse_args()
+
+pd.read_csv(input_data)
